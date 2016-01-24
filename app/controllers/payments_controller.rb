ccd6675f4c6ca79532afe564473f49996f6d183b
Drake McCabe <drakemccabe@gmail.com>
2016-01-24 20:47:26 +0000

    add thanks controller redirect

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,8 +6,9 @@ class PaymentsController < ApplicationController
       driver = payment.add_driver
       JoinMail.new(driver).send!
       flash["message"] = "Thank You For Pre Registering"
-      redirect_to events_path(params[:id])
+      redirect_to thanks_path
     else
+      flash["error"] = "Payment Failed"
       redirect_to events_path(params[:id])
     end
   end
