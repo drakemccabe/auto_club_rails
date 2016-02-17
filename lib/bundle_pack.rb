5e00e7b480f72c8d5f8edcd3d0326f301864a1c3
Drake McCabe <drakemccabe@gmail.com>
2016-02-17 01:53:58 +0000

    update bundle price

--- lib/bundle_pack.rb
+++ lib/bundle_pack.rb
@@ -3,14 +3,14 @@ class BundlePack
 
   def initialize(events)
     @events = events
-    @price = (@events.first.price + @events.last.price) - 20
+    @price = (@events.first.price + @events.last.price) - 10
     @name = @events.first.name + " and " + @events.last.name
     @dates = [@events.first.date, @events.last.date]
     @ids = [@events.last.id, @events.last.id]
   end
 
   def pack
-    @price = (@events.first.price + @events.last.price) - 20
+    @price = (@events.first.price + @events.last.price) - 10
     @name = @events.first.name + " and " + @events.last.name
     @dates = [@events.first.date, @events.last.date]
     @ids = [@events.first.id, @events.last.id]
