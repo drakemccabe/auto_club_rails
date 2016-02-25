b1ff52d7766f12b4f1c1f7c7b8c6cac362208622
Drake McCabe <drakemccabe@gmail.com>
2016-02-25 13:20:01 -0500

    creates two drivers on bundle pack

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -4,6 +4,7 @@ class DriversController < ApplicationController
 
   def index
     @events = Event.all
+    @new_driver = Driver.new
     event = Event.find(params[:event_id])
     drivers = event.drivers
     respond_to do |format|
@@ -13,7 +14,6 @@ class DriversController < ApplicationController
   end
 
   def create
-    binding.pry
     event = Event.find(params[:event_id])
     reg_driver = Driver.find(params[:driver_id])
     new_event = Event.find(params[:new_event_id])
