45e9bf3d3947129e9e99bf3d14c0b105cec1e460
Drake McCabe <drakemccabe@gmail.com>
2016-05-23 01:02:45 +0000

    remove redirect

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_filter :redirect_to_https, only: [:show]
+  #before_filter :redirect_to_https, only: [:show]
 
   def index
     @events = Event.all.order(:date).limit(20)
